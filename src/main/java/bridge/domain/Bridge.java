ec89f63524621aee7b35afaeda89941cccec3615
Nara Park <skfk6352@naver.com>
2023-12-13 23:44:30 +0900

    <feat>(Bridge): 다리의 길이와 만들어진 길을 관리하는 도메인 클래스 구현

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,18 @@
+package bridge.domain;
+
+import java.util.List;
+
+public class Bridge {
+
+    private final BridgeSize bridgeSize;
+    private final List<String> bridgeRoad;
+
+    private Bridge(int size, List<String> bridgeRoad) {
+        this.bridgeSize = BridgeSize.from(size);
+        this.bridgeRoad = bridgeRoad;
+    }
+
+    public static Bridge of(int size, List<String> bridgeRoad) {
+        return new Bridge(size, bridgeRoad);
+    }
+}
